07b4edae95d0d667d81aca3edb0d0212c1e713ce
Woochang1132 <show4031132@gmail.com>
2023-12-30 16:12:44 +0900

    shoppy-repeat => Navbar, User Component 초안

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <header>
+            <h1>Shoppy</h1>
+
+            <nav>
+                Products
+            </nav>
+        </header>
+    );
+}
+
