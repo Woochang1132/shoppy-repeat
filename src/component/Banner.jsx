6f84c83a614d491fd419ada4fb8c12df81734811
Woochang1132 <show4031132@gmail.com>
2024-01-20 12:59:30 +0900

    shoppy-repeat => banner(화면) 구성하기

---
+++ src/component/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Banner() {
+    return (
+        <section>
+           <div></div>
+           <div>
+                <h2>Shop with US</h2>
+                <p>Best Products, High Quality</p>
+           </div>
+        </section>
+    );
+}
+
